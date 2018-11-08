93d33f2931a2275613cd8b5cd49d9f6e957d80e6
jdsulkoske <jdsulkoske@bsu.edu>
2018-11-08 15:41:24 -0800

    Updated User files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Customer from './Customer';
+import User from './User';
 
 class App extends Component {
 
 	render() {
 		return (
 			<div>
-				<Customer />
+				<User />
 			</div>
 		)
 	}
